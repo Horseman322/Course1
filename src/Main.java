5f8a3baa1c76887d4e237236dc79a3e6f2cf0b3a
Kosarev <kosarevn97@gmail.com>
2022-08-16 23:22:44 +0700

    Ступор

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
-    public static void main(String[] args) {
+    private static final Employee[] employees = new Employee[10];
 
+    public static int calculateTotalSalary() {
+        int sum = 0;
+        for (Employee employee : employees)
+            sum += employee.getSalary();
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Сергей", "Петрович", "Большов", 1, 80000);
+        Employee emp2 = new Employee("Михаил", "Семенович", "Пальчиков", 1, 60000);
+        Employee emp3 = new Employee("Светослав", "Валерьевич", "Мышенок", 1, 100000);
+        Employee emp4 = new Employee("Олег", "Олегович", "Олегов", 1, 73234);
+        Employee emp5 = new Employee("Павел", "Аркадьевич", "Младенцов", 1, 99999);
+
+        employees[0] = emp1;
+        employees[1] = emp2;
+        employees[2] = emp3;
+        employees[3] = emp4;
+        employees[4] = emp5;
+
+        System.out.println(emp1);
+        System.out.println();
+        System.out.println(emp2);
+        System.out.println();
+        System.out.println(emp3);
+        System.out.println();
+        System.out.println(emp4);
+        System.out.println();
+        System.out.println(emp5);
+        System.out.println();
+        System.out.println();
+        System.out.println(calculateTotalSalary());
     }
 }
